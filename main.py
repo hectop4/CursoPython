edc5671ddccb6c8eda013ba575e33fbef3e65d5d
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2023-04-03 21:09:52 -0500

    Hola

--- main.py
+++ main.py
@@ -7,7 +7,24 @@ print(12**2, 'El mundo')
 
 # Hola Mundo en comentario
 
-"""
-Super 
-Comentario
-"""
+
+
+#string
+my_name="Hector"
+
+print(my_name)
+#Int
+my_age=12
+
+print(my_age)
+
+my_name="HEctoraso"
+print("Cambios puedo hacer: ",my_name)
+
+
+#input
+
+my_name=input("Cual es tu nombre? ")
+print(my_name)
+
+
