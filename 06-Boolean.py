d5a67627502a5babe8ccb171977dacb94c4146b5
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2023-04-05 19:42:40 -0500

    Dia 3 de clases

---
+++ 06-Boolean.py
@@ -0,0 +1,9 @@
+is_single=True
+print(type(is_single))
+print(is_single)
+is_single=False
+print(type(is_single))
+print(is_single)
+is_single=not is_single
+is_single=True and is_single
+print(is_single)
---
+++ 07-transformarTipos.py
@@ -0,0 +1,10 @@
+name='Hector'
+print(type(name))
+name=12
+print(type(name))
+name=True
+print(type(name))
+
+print('Hello World'+ ' ' + str(name)) 
+print(10+20)
+
