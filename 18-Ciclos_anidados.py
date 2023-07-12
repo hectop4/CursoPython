734fb0647236e2d7bb97edc00dd2c6e3f70c84a7
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2023-07-11 19:44:18 -0500

    Agregando ciclos anidados

---
+++ 18-Ciclos_anidados.py
@@ -0,0 +1,14 @@
+#los Ciclos anidados son ciclos dentro de ciclos
+#Ejemplo:
+matriz= [[1,2,3],
+         [4,5,6],
+         [7,8,9]
+        ]
+print(matriz[0][1])
+
+for row in matriz:
+    print(row)
+    for column in row:
+        print(column)
+
+
